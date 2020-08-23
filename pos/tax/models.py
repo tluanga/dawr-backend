0ed1fc43d64edb78c365d917f13c39fe052648f2
tluanga <tluanga@live.com>
2020-08-23 18:57:26 +0530

    a

--- pos/tax/models.py
+++ pos/tax/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class GSTCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
     cgst=models.FloatField()
-    sgst=models.FloatField()
-    remarks = models.CharField(max_length=250)
+    sgst = models.FloatField()
+    totalGst=models.FloatField()
+    description=models.CharField(max_length=500)
+    remarks = models.CharField(max_length=250,blank=True, null=True)
+    active=models.BooleanField(default=True)
     
     
     def CalculateTax(self,amount):
@@ -13,6 +16,8 @@ class GSTCode(models.Model):
         tax=(amount/100)*tax_percentage
         return tax
         
-
+    def save(self, *args, **kwargs):
+       self.totalGst=self.cgst+self.sgst
+       super(GSTCode, self).save(*args, **kwargs) # Call the real save() method
     def __str__(self):
         return self.code
