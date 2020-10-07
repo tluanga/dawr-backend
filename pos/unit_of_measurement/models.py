7c377f803ddd1a83673b50a9b958d46043d54208
tluanga <tluanga@live.com>
2020-10-07 14:37:44 +0530

    unit of measurement mock data completed

--- pos/unit_of_measurement/models.py
+++ pos/unit_of_measurement/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class UnitOfMeasurement(models.Model):
     active = models.BooleanField(default=True)
-    name = models.CharField(max_length=255)
-    measurement_type = models.CharField(max_length=255)
+    unit_of_measurement = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=255)
+    type_of_measurement=models.CharField(max_length=255)
+    
 
     def __str__(self):
         return self.name
